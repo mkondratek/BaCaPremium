1c17dce8189d416ca684f3c16640177ffa523768
Mikołaj Kondratek <mik.kondratek@gmail.com>
2018-01-02 00:42:50 +0100

    Using jquery now (place it in /js).

--- ext/js/general.js
+++ ext/js/general.js
@@ -1,3 +1,10 @@
+
+/* global variables */
+var __current_href = window.location.href;
+var __isuptodate = false;
+var __attempts = 0;
+/*  ^ ^ ^ ^ ^ ^ ^   */
+
 function makeInfo() {
     var info = document.createElement('div');
     info.className = 'info';
@@ -5,21 +12,51 @@ function makeInfo() {
 }
 
 function setInfo() {
-    var info = document.getElementsByClassName('info');
-    info[0].innerHTML = 'Work in progress!<br>' + __current_href;
+    $(".info").html('Work in progress!<br>' + __current_href);
 }
 
-/* global variables */
-var __current_href = window.location.href;
-/*  ^ ^ ^ ^ ^ ^ ^   */
-
-makeInfo();
-
-window.onclick = function () {
-    __current_href = window.location.href;
-    setInfo();
-
-    if (__current_href.match('(ProblemStatistics|ResultsPanel)') !== null) {
-        setTimeout(fixThemAll(), 800);
+function executor(__name, __func) {
+    if ($(__name).is(':visible')) {
+        console.log("READY!");
+        __attempts = 0;
+        __func();
+    } else if (__attempts < 100) {
+        console.log("W8ING: " + __attempts);
+        __attempts++;
+        setTimeout(executor(__name, __func), 50);
+    } else {
+        console.log("ABORTED!");
+        __attempts = 0;
     }
-};
+}
+
+function run() {
+    if (!__isuptodate) {
+        console.log(new Date().toString());
+        __current_href = window.location.href;
+        setInfo();
+
+        if (__current_href.match('(ProblemStatistics|ResultsPanel)') !== null) {
+            executor('tr.testerka-th', fixThemAll);
+        }
+
+        __isuptodate = true;
+    }
+}
+
+$(document).click( function () {
+    __isuptodate = false;
+    run();
+});
+
+$(document).ready( function() {
+    makeInfo();
+
+    // var s = document.createElement("script");
+    // s.defer = true;
+    // s.innerHTML = "run();";
+    // var __cnt = 0;
+    //  while (__cnt++ < 100) { setTimeout(run, 50); }
+    //
+    // document.body.appendChild(s);
+});
